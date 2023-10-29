a08239965779d49cbc63cc4597a68d7579cb2632
Thanakit24 <univgworld@gmail.com>
2023-10-29 21:43:16 +0700

    Character Movement Redfined

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,30 +1,60 @@
 using System;
 using UnityEngine;
 
-namespace TarodevController
+namespace unixel
 {
- 
-    [RequireComponent(typeof(Rigidbody2D), typeof(Collider2D))]
     public class PlayerController : MonoBehaviour, IPlayerController
     {
-        [SerializeField] private ScriptableStats _stats;
+        public LayerMask playerLayer;
         private Rigidbody2D rb;
         private CapsuleCollider2D col;
-        private FrameInput _frameInput;
-        private Vector2 _frameVelocity;
+
+        [Header("MOVEMENT")]
+        private Vector2 moveDirection;
+        public float maxSpeed;
+        public float acceleration;
+        public float groundDeceleration;
+        public float airDeceleration;
+        public float maxFallSpeed;
+        public float fallAcceleration;
+        public float groundingForce;
+
+        [Header("GROUND & COLLISION CHECKS")]
+        public LayerMask groundLayerMask;
+        public float grounderDistance;
+        public float JumpEndEarlyGravityModifier;
         private bool _cachedQueryStartInColliders;
 
-        #region Interface
+        [Header("JUMPING")]
+        public float jumpPower;
+        private bool jumpToConsume;
+        private float timeJumpWasPressed;
+        private bool bufferedJumpUsable;
+        public float jumpBuffer;
+        private bool endedJumpEarly;
+        private bool coyoteUsable;
+        public float coyoteTime;
 
-        public Vector2 FrameInput => _frameInput.Move;
+        [Header("GLIDING")]
+        public float maxGlideSpeed;
+        public float glideAcceleration;
+        public float glideFallSpeed;
+        public float glideFallAcceleration;
+        public float glideGravityResistance;
+
+        private Vector2 frameVelocity;
+        private FrameInput frameInput;
+        public bool isFacingRight = true;
+
+        private float time;
+
+        //Interface
+        public Vector2 FrameInput => frameInput.Move;
         public event Action<bool, float> GroundedChanged;
         public event Action Jumped;
 
-        #endregion
-
-        private float _time;
-
-        private void Awake()
+        // Start is called before the first frame update
+        void Awake()
         {
             rb = GetComponent<Rigidbody2D>();
             col = GetComponent<CapsuleCollider2D>();
@@ -32,32 +62,35 @@ namespace TarodevController
             _cachedQueryStartInColliders = Physics2D.queriesStartInColliders;
         }
 
-        private void Update()
+        // Update is called once per frame
+        void Update()
         {
-            _time += Time.deltaTime;
+            time += Time.deltaTime;
             GatherInput();
+            //if (frameInput.isGliding)
+            //{
+            //    print("in gliding state");
+            //}
         }
+
 
         private void GatherInput()
         {
-            _frameInput = new FrameInput
+            frameInput = new FrameInput
             {
-                JumpDown = Input.GetButtonDown("Jump") || Input.GetKeyDown(KeyCode.C),
-                JumpHeld = Input.GetButton("Jump") || Input.GetKey(KeyCode.C),
+                JumpDown = Input.GetButtonDown("Jump"),
+                JumpHeld = Input.GetButton("Jump"),
+                isGliding = Input.GetKey(KeyCode.LeftShift),
                 Move = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"))
             };
 
-            if (_stats.SnapInput) //preventing gamepads from moving slower
-              {
-                _frameInput.Move.x = Mathf.Abs(_frameInput.Move.x) < _stats.HorizontalDeadZoneThreshold ? 0 : Mathf.Sign(_frameInput.Move.x);
-                _frameInput.Move.y = Mathf.Abs(_frameInput.Move.y) < _stats.VerticalDeadZoneThreshold ? 0 : Mathf.Sign(_frameInput.Move.y);
+            if (frameInput.JumpDown)
+            {
+                jumpToConsume = true;
+                timeJumpWasPressed = time;
             }
 
-            if (_frameInput.JumpDown)
-            {
-                _jumpToConsume = true;
-                _timeJumpWasPressed = _time;
-            }
+            FlipSprite();
         }
 
         private void FixedUpdate()
@@ -70,128 +103,133 @@ namespace TarodevController
             ApplyMovement();
         }
 
+        private void HandleDirection()
+        {
+            if (frameInput.isGliding)
+            {
+                frameVelocity.x = Mathf.MoveTowards(frameVelocity.x, frameInput.Move.x * maxGlideSpeed, glideAcceleration * Time.fixedDeltaTime);
+            }
+
+            else if  (frameInput.Move.x == 0)
+            {
+                var deceleration = grounded ? groundDeceleration : airDeceleration;
+                // this line of code is setting the deceleration variable to either groundDeceleration or airDeceleration
+                // based on whether the character is currently on the ground (grounded).
+                // If grounded is true, it will use groundDeceleration; otherwise, it will use airDeceleration
+                frameVelocity.x = Mathf.MoveTowards(frameVelocity.x, 0, deceleration * Time.fixedDeltaTime);
+            }
+            else
+            {
+                frameVelocity.x = Mathf.MoveTowards(frameVelocity.x, frameInput.Move.x * maxSpeed, acceleration * Time.fixedDeltaTime);
+            }
+        }
+
+        private void HandleGravity()
+        {
+            if (grounded && frameVelocity.y <= 0f)
+            {
+                frameVelocity.y = groundingForce;
+            }
+            else if (frameInput.isGliding)
+            {
+                frameVelocity.y /= glideGravityResistance;
+                frameVelocity.y = Mathf.MoveTowards(frameVelocity.y, -glideFallSpeed, glideFallAcceleration * Time.fixedDeltaTime);
+            }
+            else
+            {
+                var inAirGravity = fallAcceleration;
+                if (endedJumpEarly && frameVelocity.y > 0) inAirGravity *= JumpEndEarlyGravityModifier;
+                frameVelocity.y = Mathf.MoveTowards(frameVelocity.y, -maxFallSpeed, inAirGravity * Time.fixedDeltaTime);
+            }
+            
+        }
+        private void ApplyMovement() => rb.velocity = frameVelocity;
+
+
         #region Collisions
 
-        private float _frameLeftGrounded = float.MinValue;
-        private bool _grounded;
+        private float frameLeftGrounded = float.MinValue;
+        private bool grounded;
 
-        private void CheckCollisions()
+        private void CheckCollisions() //confusing on the GroundChanged?
         {
             Physics2D.queriesStartInColliders = false;
 
             // Ground and Ceiling
-            bool groundHit = Physics2D.CapsuleCast(col.bounds.center, col.size, col.direction, 0, Vector2.down, _stats.GrounderDistance, ~_stats.PlayerLayer);
-            bool ceilingHit = Physics2D.CapsuleCast(col.bounds.center, col.size, col.direction, 0, Vector2.up, _stats.GrounderDistance, ~_stats.PlayerLayer);
+            bool groundHit = Physics2D.CapsuleCast(col.bounds.center, col.size, col.direction, 0, Vector2.down, grounderDistance, ~playerLayer);
+            bool ceilingHit = Physics2D.CapsuleCast(col.bounds.center, col.size, col.direction, 0, Vector2.up, grounderDistance, ~playerLayer);
 
             // Hit a Ceiling
-            if (ceilingHit) _frameVelocity.y = Mathf.Min(0, _frameVelocity.y);
+            if (ceilingHit) frameVelocity.y = Mathf.Min(0, frameVelocity.y);
 
             // Landed on the Ground
-            if (!_grounded && groundHit)
+            if (!grounded && groundHit)
             {
-                _grounded = true;
-                _coyoteUsable = true;
-                _bufferedJumpUsable = true;
-                _endedJumpEarly = false;
-                GroundedChanged?.Invoke(true, Mathf.Abs(_frameVelocity.y));
+                grounded = true;
+                coyoteUsable = true;
+                bufferedJumpUsable = true;
+                endedJumpEarly = false;
+                GroundedChanged?.Invoke(true, Mathf.Abs(frameVelocity.y)); //idk wtf this is
             }
             // Left the Ground
-            else if (_grounded && !groundHit)
+            else if (grounded && !groundHit)
             {
-                _grounded = false;
-                _frameLeftGrounded = _time;
+                grounded = false;
+                frameLeftGrounded = time;
                 GroundedChanged?.Invoke(false, 0);
             }
 
             Physics2D.queriesStartInColliders = _cachedQueryStartInColliders;
+
         }
-
         #endregion
-
-
         #region Jumping
 
-        private bool _jumpToConsume;
-        private bool _bufferedJumpUsable;
-        private bool _endedJumpEarly;
-        private bool _coyoteUsable;
-        private float _timeJumpWasPressed;
-
-        [SerializeField] private bool HasBufferedJump => _bufferedJumpUsable && _time < _timeJumpWasPressed + _stats.JumpBuffer;
-        [SerializeField] private bool CanUseCoyote => _coyoteUsable && !_grounded && _time < _frameLeftGrounded + _stats.CoyoteTime;
+        [SerializeField] private bool HasBufferedJump => bufferedJumpUsable && time < timeJumpWasPressed + jumpBuffer; //buffering jump before performing next jump
+        [SerializeField] private bool CanUseCoyote => coyoteUsable && !grounded && time < frameLeftGrounded + coyoteTime; 
 
         private void HandleJump() //Jumping conditions 
         {
-            if (!_endedJumpEarly && !_grounded && !_frameInput.JumpHeld && rb.velocity.y > 0) _endedJumpEarly = true;
+            if (!endedJumpEarly && !grounded && !frameInput.JumpHeld && rb.velocity.y > 0) 
+                endedJumpEarly = true;
 
-            if (!_jumpToConsume && !HasBufferedJump) return;
+            if (!jumpToConsume && !HasBufferedJump) 
+                return;
 
-            if (_grounded || CanUseCoyote) ExecuteJump();
+            if (!frameInput.isGliding && jumpToConsume && grounded || CanUseCoyote) 
+                ExecuteJump();
 
-            _jumpToConsume = false;
+            jumpToConsume = false;
         }
 
         private void ExecuteJump() //Where jump happens
         {
-            _endedJumpEarly = false;
-            _timeJumpWasPressed = 0;
-            _bufferedJumpUsable = false;
-            _coyoteUsable = false;
-            _frameVelocity.y = _stats.JumpPower;
+            endedJumpEarly = false;
+            timeJumpWasPressed = 0;
+            bufferedJumpUsable = false;
+            coyoteUsable = false;
+            frameVelocity.y = jumpPower;
             Jumped?.Invoke();
         }
 
         #endregion
-
-        #region Horizontal
-
-        private void HandleDirection()
+        private void FlipSprite()
         {
-            if (_frameInput.Move.x == 0)
+            if (isFacingRight && moveDirection.x < 0f || !isFacingRight && moveDirection.x > 0f)
             {
-                var deceleration = _grounded ? _stats.GroundDeceleration : _stats.AirDeceleration;
-                _frameVelocity.x = Mathf.MoveTowards(_frameVelocity.x, 0, deceleration * Time.fixedDeltaTime);
-            }
-            else
-            {
-                _frameVelocity.x = Mathf.MoveTowards(_frameVelocity.x, _frameInput.Move.x * _stats.MaxSpeed, _stats.Acceleration * Time.fixedDeltaTime);
+                isFacingRight = !isFacingRight;
+                Vector3 localScale = transform.localScale;
+                localScale.x *= -1f;
+                transform.localScale = localScale;
             }
         }
 
-        #endregion
-
-        #region Gravity
-
-        private void HandleGravity()
-        {
-            if (_grounded && _frameVelocity.y <= 0f)
-            {
-                _frameVelocity.y = _stats.GroundingForce;
-            }
-            else
-            {
-                var inAirGravity = _stats.FallAcceleration;
-                if (_endedJumpEarly && _frameVelocity.y > 0) inAirGravity *= _stats.JumpEndEarlyGravityModifier;
-                _frameVelocity.y = Mathf.MoveTowards(_frameVelocity.y, -_stats.MaxFallSpeed, inAirGravity * Time.fixedDeltaTime);
-            }
-        }
-
-        #endregion
-
-        private void ApplyMovement() => rb.velocity = _frameVelocity;
-
-#if UNITY_EDITOR
-        private void OnValidate()
-        {
-            if (_stats == null) Debug.LogWarning("Please assign a ScriptableStats asset to the Player Controller's Stats slot", this);
-        }
-#endif
     }
-
     public struct FrameInput
     {
         public bool JumpDown;
         public bool JumpHeld;
+        public bool isGliding;
         public Vector2 Move;
     }
 
