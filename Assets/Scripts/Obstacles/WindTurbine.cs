22545c8596526ac2993751637104db4321bc387a
Thanakit24 <univgworld@gmail.com>
2023-12-01 19:41:58 +0700

    Level 6

--- Assets/Scripts/Obstacles/WindTurbine.cs
+++ Assets/Scripts/Obstacles/WindTurbine.cs
@@ -32,19 +32,20 @@ public class WindTurbine : MonoBehaviour
             Rigidbody2D rbAffect = collision.gameObject.GetComponent<Rigidbody2D>();
             rbAffect.velocity = Vector2.zero;
             rbAffect.gravityScale = 0f;
-            rbAffect.AddForce(player.rb.transform.up * upwardForce, ForceMode2D.Force);
+            rbAffect.AddForce(-player.rb.transform.up * upwardForce, ForceMode2D.Force);
             //player.rb.transform.up
         }
     }
-
+                                                                                                                                                    
     private void OnTriggerExit2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
-            //print("Player Exit Wind");
+            //print("Player Exit Wind");                                                                                                                                                                                                                                                                                                                                
             PlayerController player = collision.gameObject.GetComponent<PlayerController>();
             Rigidbody2D rbAffect = collision.gameObject.GetComponent<Rigidbody2D>();
             //rbAffect.velocity = Vector2.zero;
+            player.frameVelocity.y = 0;                                                                                                                 
             player.jumpPower = 13.2f;
             player.flyUpwardSpeed = 250f;
             rbAffect.gravityScale = 1f;
