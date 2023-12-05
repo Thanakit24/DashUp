018ab1386aa492dd72f2a5caed60804f3dfd689d
Thanakit24 <univgworld@gmail.com>
2023-12-05 21:56:59 +0700

    Level 10

---
+++ Assets/Scripts/Menu/LevelSelection.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelSelection : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void LevelSelect(int levelNumber)
+    {
+        Debug.Log($"{levelNumber} lvl number");
+        SceneManager.LoadScene(levelNumber + 1);
+    }
+
+    public void Back()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+}
